6162604d07d619a4d8937606e296bb68572c0541
tetetearraio916 <notfase1192555@gmail.com>
2020-09-19 16:08:04 +0900

    post_controllerの設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,14 +13,24 @@ class PostsController < ApplicationController
       redirect_to posts_path, success: "投稿しました"
     else
       flash[:danger] = "投稿に失敗しました"
-      render new
+      render :new
   end
 
   def edit
-    @post = Post.find(params)
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to edit_post_path, success: "更新しました"
+    else
+      flash[:danger] = "更新に失敗しました"　
+    　render :edit
   end
 
   def show
+    @post.find(params[:id])
   end
 
   private
