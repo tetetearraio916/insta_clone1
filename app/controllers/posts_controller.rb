cf0b5f2e82756cb45a10e658fe2db186d5ea436b
tetetearraio916 <notfase1192555@gmail.com>
2020-09-19 16:05:18 +0900

    Postのルーティング設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,31 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, success: "投稿しました"
+    else
+      flash[:danger] = "投稿に失敗しました"
+      render new
   end
 
   def edit
+    @post = Post.find(params)
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    require(:post).permit(:content, :image)
   end
 end
