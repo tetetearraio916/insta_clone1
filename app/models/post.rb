6e25d9ae69eeed26a915af7df2da747847345500
tetetearraio916 <notfase1192555@gmail.com>
2020-09-19 15:55:36 +0900

    postモデルにimageカラム追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  content    :text(65535)
+#  image      :string(255)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_063311) do
+ActiveRecord::Schema.define(version: 2020_09_19_065303) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
