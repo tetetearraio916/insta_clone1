a00eb92fbe35ff4a62132f7aabc346afa6bc3179
tetetearraio916 <notfase1192555@gmail.com>
2020-11-02 18:47:42 +0900

    add comment for comment model
    コメント機能実装において、メモがてらにコメントを追加しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
+  #shallowを使う事でurlの省略
   resources :posts, shallow: true do
     resources :comments, only: [:create, :edit, :update, :destroy]
   end
