54b62c825ab4daad57f96b55829acbdd1a40eefb
tetetearraio916 <notfase1192555@gmail.com>
2020-11-30 12:50:11 +0900

    add routing of account in mypage
    プロフィール用にmypage/accountというルーティングを設定しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
-  
+
   resources :likes, only: [:create, :destroy]
 
-  #shallowを使う事でurlの省略
+  #shallowを使う事でurlの親のidを省略
   resources :posts, shallow: true do
     resources :comments, only: [:create, :edit, :update, :destroy]
   end
 
   resources :users
+
+  scope :mypage do
+    #今回はurlにidが不要なためresourcesではなく、resourceを使う
+    resource :account, only: :edit
+  end
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
