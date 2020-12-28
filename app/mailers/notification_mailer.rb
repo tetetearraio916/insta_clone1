c5bf13ddc54d217f70bb8b6bd7e3dcb2d6d111e9
tetetearraio916 <notfase1192555@gmail.com>
2020-12-28 15:23:14 +0900

    setting notification_mailer
    通知用mailerの実装を行いました。フォロー、いいね、コメントによって件名が変わるように実装。

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -5,9 +5,24 @@ class NotificationMailer < ApplicationMailer
   #
   #   en.notification_mailer.send_notification.subject
   #
-  def send_notification
-    @greeting = "Hi"
+  default from: "instaclon@example.com"
 
-    mail to: "to@example.org"
+  def send_notification(notification)
+    @notification = notification
+    mail subject: what_action_type?(@notification)
+         date: @notification.created_at
+         to: @notification.user.email
   end
+
+  private
+
+  def what_action_type?(notification)
+    case notification.action_type.to_sym
+      when :commented_to_own_post
+        "#{notification.subject.user.name}さんがあなたの投稿にコメントしました"
+      when :liked_to_own_post
+        "#{notification.suject.user.name}さんがあなたの投稿にいいねしました"
+      when :followed_me
+        "#{notification.subject.follow.name}さんがあなたをフォローしました"
+    end
 end
