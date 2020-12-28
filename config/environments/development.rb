944572582c94b6d0c270f97e5dd2e5c36dce7f77
tetetearraio916 <notfase1192555@gmail.com>
2020-12-28 14:42:51 +0900

    setting development.rb
    config/development.rb内でメール送信の設定を加えました。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,18 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            '荒井信輝',
+    password:             'zyouhoudaisuki.com@gmail.com',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+
 
   config.action_mailer.perform_caching = false
 
