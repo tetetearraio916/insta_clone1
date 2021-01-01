415d4a2ff71730d2e2d90c8a7925fd3d2c9aa8cb
tetetearraio916 <notfase1192555@gmail.com>
2021-01-01 23:07:28 +0900

    :sparkles: add letter_opener_web for routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   resources :likes, only: [:create, :destroy]
 
   #shallowを使う事でurlの親のidを省略
