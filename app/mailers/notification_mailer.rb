3b2d369db9c2539c10db7ba5e2f49d2f939f1326
tetetearraio916 <notfase1192555@gmail.com>
2021-01-02 18:16:01 +0900

    Fix typos

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,20 +4,20 @@ class NotificationMailer < ApplicationMailer
     @user_from = params[:user_from]
     @user_to = params[:user_to]
     @post = params[:post]
-    mail(to: @user_to.email, subject: "#{@user_from.username}があなたの投稿にいいねしました")
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました")
   end
 
   def comment_post
     @user_from = params[:user_from]
     @user_to = params[:user_to]
     @comment = params[:comment]
-    mail(to: @user_to.email, subject: "#{@user_from.username}があなたの投稿にいいねしました")
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたの投稿にいいねしました")
   end
 
   def follow
     @user_from = params[:user_from]
     @user_to = params[:user_to]
-    mail(to: @user_to.email, subject: "#{@user_from.username}があなたをフォローしました")
+    mail(to: @user_to.email, subject: "#{@user_from.name}があなたをフォローしました")
   end
 
 
