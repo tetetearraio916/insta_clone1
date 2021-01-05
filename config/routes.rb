2f7ef1d970fa91ca628550f5e380928a686a34aa
tetetearraio916 <notfase1192555@gmail.com>
2021-01-05 16:41:59 +0900

    Fix sidekiq in routes.rb
    development環境のみsidekiqのダッシュボードが接続できるように設定を修正しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+    require 'sidekiq/web'
+    mount Sidekiq::Web, at: "/sidekiq"
+  end
 
 
-  require 'sidekiq/web'
-  mount Sidekiq::Web, at: "/sidekiq"
 
 
 
