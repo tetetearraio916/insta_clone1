7a90175e68a789b5392b35052cdb2d0d852a87f8
tetetearraio916 <notfase1192555@gmail.com>
2021-01-25 17:31:34 +0900

    add notification to users
    usersテーブルにcomment、like、followの通知を設定するカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_25_105252) do
+ActiveRecord::Schema.define(version: 2021_01_25_082804) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content", null: false
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 2020_12_25_105252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.boolean "notification_on_comment", default: false
+    t.boolean "notification_on_like", default: false
+    t.boolean "notification_on_follow", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
