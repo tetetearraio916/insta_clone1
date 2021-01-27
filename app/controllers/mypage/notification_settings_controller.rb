bfe21b8c0058324e417b2a4807efca988a8c4c94
tetetearraio916 <notfase1192555@gmail.com>
2021-01-27 11:12:00 +0900

    fix notification_settings_controller
    余計なメソッドを削除。

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -7,7 +7,7 @@ class Mypage::NotificationSettingsController < ApplicationController
 
   def update
     @user = User.find(current_user.id)
-    if @user.update(params_int(notification_params))
+    if @user.update(notification_params)
       redirect_to edit_mypage_notification_setting_path, success: "設定を更新しました"
     else
       flash[:danger] ="設定の更新に失敗しました"
@@ -17,25 +17,8 @@ class Mypage::NotificationSettingsController < ApplicationController
 
   private
 
-
   def notification_params
     params.require(:user).permit(:notification_on_comment, :notification_on_like, :notification_on_follow)
   end
 
-  #データ型がstringかintegerか判断する
-  def integer_string?(str)
-   Integer(str)
-   true
-  rescue ArgumentError
-   false
-  end
-
-  #paramsのvalueをstringからintegerにする
-  def params_int(model_params)
-    model_params.each do |key,value|
-      if integer_string?(value)
-        model_params[key]=value.to_i
-      end
-    end
-  end
 end
