b7921480339d0f65b932edb7b9f9a47caa3f6091
tetetearraio916 <notfase1192555@gmail.com>
2021-01-29 12:50:16 +0900

    add metatag setting to helper
    application_helper.rbに,metatagのdefault設定を記述。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+        title: 'InstaClone - Railsの実践的アプリケーション',
+        description: 'Ruby on Railsの実践的な課題です。Sidekiqを使った非同期処理やトランザクションを利用した課金処理など実践的な内容が学べます。',
+        charset: 'utf-8',
+        keywords: ['rails', 'instaclone', 'rails特訓コース'],
+        reverse: true,
+        og: {
+          image: image_url('/images/default.png'),
+        },
+    }
+
+  end
 end
