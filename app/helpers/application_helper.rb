06fc70d5b222b759a4e4d8897d88b5378c5023a7
tetetearraio916 <notfase1192555@gmail.com>
2021-01-31 20:01:25 +0900

    fix helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,29 @@
 module ApplicationHelper
   def default_meta_tags
     {
-        title: 'InstaClone - Railsの実践的アプリケーション',
-        description: 'Ruby on Railsの実践的な課題です。Sidekiqを使った非同期処理やトランザクションを利用した課金処理など実践的な内容が学べます。',
-        charset: 'utf-8',
-        keywords: ['rails', 'instaclone', 'rails特訓コース'],
+        site: Settings.meta.site,
+        # trueに設定すると「title | site」の並びで出力
         reverse: true,
+        title: Settings.meta.title,
+        description: Settings.meta.description,
+        keywords: Settings.meta.keywords,
+        # URLを正規化するcanonicalタグを設定
+        canonical: request.original_url,
+        #OGPの設定
         og: {
-          image: image_url('/images/default.png'),
+            title: :full_title,
+            #ウェブサイト、記事、ブログサイトの種類
+            type: Settings.meta.og.type,
+            url: request.original_url,
+            image: image_url(Settings.meta.og.image_path),
+            site_name: :site,
+            description: :description,
+            # リソース言語を設定
+            locale: 'ja_JP'
+        },
+        twitter: {
+            #twitterOGPのSummaryカード
+            card: 'summary_large_image'
         },
     }
   end
