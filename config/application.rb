1156b282d30b8fb13b1fc0c571b773014b04f928
tetetearraio916 <notfase1192555@gmail.com>
2021-02-14 02:46:45 +0900

    :gear: change 余計なテストファイルが生成されないよう設定
    @tetetearraio916

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ module InstaClone1
       g.skip_routes true # trueならroutes.rb変更せず、falseなら通常通り変更
       g.test_framework false # testファイル生成せず
       g.helper false
+      g.test_framework :rspec,
+        fixtures: false,
+        controller_specs: false ,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
     end
 
     config.active_job.queue_adapter = :sidekiq
