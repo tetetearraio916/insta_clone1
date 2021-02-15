f412c2a9aaafb80ef37c03458a0955ab205eeb25
tetetearraio916 <notfase1192555@gmail.com>
2021-02-15 14:49:54 +0900

    :bug: fix migrationファイルとデータベースの整合性を整える
    @tetetearraio916

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@
 #
 #  id         :bigint           not null, primary key
 #  content    :text(65535)
+#  images     :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :bigint
