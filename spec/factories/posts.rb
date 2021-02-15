caeb0f5280bb095a93700478c3bf2d03f136d29e
tetetearraio916 <notfase1192555@gmail.com>
2021-02-15 14:54:24 +0900

    :bug: postのテストデータを修正
    @tetetearraio916

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,27 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
+#  images     :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 FactoryBot.define do
   factory :post do
-    association :user
     content { Faker::Lorem.word }
+    #ハマった部分、後で原因を調べる
+    images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/IMG_52CBCD9693AC-1.jpeg'), 'spec/fixtures/IMG_52CBCD9693AC-1.jpeg') ] }
+    user
   end
 end
