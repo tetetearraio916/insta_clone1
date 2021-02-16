0685e41b8dcf749d56620aa3ace6d8247f57ba9c
tetetearraio916 <notfase1192555@gmail.com>
2021-02-16 14:37:33 +0900

    :bug: fix テスト画像の名称わかりにくいので変更
    @tetetearraio916

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -21,11 +21,11 @@ FactoryBot.define do
   factory :post do
     content { Faker::Lorem.word }
     #複数画像保存の際に、json形式で保存するため対応した([]に値を入れなければならない)
-    images { [File.open("#{Rails.root}/spec/fixtures/IMG_52CBCD9693AC-1.jpeg")] }
+    images { [File.open("#{Rails.root}/spec/fixtures/dummy.jpeg")] }
     user
 
     trait :multiple_images do
-      images { [File.open("#{Rails.root}/spec/fixtures/IMG_52CBCD9693AC-1.jpeg"), File.open("#{Rails.root}/spec/fixtures/IMG_52CBCD9693AC-1.jpeg")] }
+      images { [File.open("#{Rails.root}/spec/fixtures/dummy.jpeg"), File.open("#{Rails.root}/spec/fixtures/dummy.jpeg")] }
     end
 
   end
