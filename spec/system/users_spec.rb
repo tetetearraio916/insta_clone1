fe8a680a712b0e44fcf9eebf5b43743c111a3e61
tetetearraio916 <notfase1192555@gmail.com>
2021-02-17 03:34:36 +0900

    :bug: fix ユーザーのシステムスペックの修正
    @tetetearraio916

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,46 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  let!(:user) { create(:user) }
-  let!(:other_user) { create(:user) }
-
 
   describe 'ユーザー登録' do
-    it 'ユーザー登録が成功すること' do
-      visit '/users/new'
-      fill_in 'user_name', with: 'tetete'
-      fill_in 'user_email', with: 'tetete@example.com'
-      fill_in 'user_password', with: 'password'
-      fill_in 'user_password_confirmation', with: 'password'
-      click_on '登録'
-      expect(page).to have_content('ユーザーの作成に成功しました')
+    context 'ユーザー情報が正しい場合' do
+      it 'ユーザー登録が成功すること' do
+        visit '/users/new'
+        fill_in 'user_name', with: 'tetete'
+        fill_in 'user_email', with: 'tetete@example.com'
+        fill_in 'user_password', with: 'password'
+        fill_in 'user_password_confirmation', with: 'password'
+        click_on '登録'
+        expect(current_path).to eq root_path
+        expect(page).to have_content('ユーザーの作成に成功しました')
+      end
     end
 
-    it 'ユーザー登録が失敗すること' do
-      visit '/users/new'
-      fill_in 'user_name', with: 'tetete'
-      fill_in 'user_email', with: 'tetete@example.com'
-      fill_in 'user_password', with: 'password'
-      fill_in 'user_password_confirmation', with: 'passwordpassword'
-      click_on '登録'
-      expect(page).to have_content('ユーザーの作成に失敗しました')
+    context 'ユーザー情報が誤りがある場合' do
+      it 'ユーザー登録が失敗すること' do
+        visit '/users/new'
+        fill_in 'user_name', with: ''
+        fill_in 'user_email', with: ''
+        fill_in 'user_password', with: ''
+        fill_in 'user_password_confirmation', with: ''
+        click_on '登録'
+        expect(page).to have_content('ユーザー名を入力してください')
+        expect(page).to have_content('メールアドレスを入力してください')
+        expect(page).to have_content('パスワードは3文字以上で入力してください')
+        expect(page).to have_content('パスワード(確認)を入力してください')
+        expect(page).to have_content('ユーザーの作成に失敗しました')
+      end
     end
   end
 
   describe 'フォロー' do
-    it 'フォローができること' do
+    let!(:user) { create(:user) }
+    let!(:other_user) { create(:user) }
+
+    before do
       login(user)
-      visit '/users'
-      find("#follow-area-#{other_user.id}").click_on("follow")
-      expect(page).to have_css "#unfollow"
+    end
+
+    it 'フォローができること' do
+      visit root_path
+      expect{
+        within "#follow-area-#{other_user.id}" do
+          click_on 'follow'
+          expect(page).to have_css('#unfollow')
+        end
+      }.to change(user.follows, :count).by(1)
     end
 
     it 'フォローを外せること' do
-      login(user)
       user.follow(other_user)
-      visit '/users'
-      find("#follow-area-#{other_user.id}").click_on("unfollow")
-      expect(page).to have_css "#follow"
+      visit root_path
+      expect{
+        within "#follow-area-#{other_user.id}" do
+          click_on 'unfollow'
+          expect(page).to have_css('#follow')
+        end
+      }.to change(user.follows, :count).by(-1)
     end
   end
 end
