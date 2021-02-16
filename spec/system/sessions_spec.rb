11a397fd171390878ba263f7453fddd52e2805fd
tetetearraio916 <notfase1192555@gmail.com>
2021-02-17 03:10:55 +0900

    :bug: fix ログインのシステムスペックを修正
    @tetetearraio916

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -4,12 +4,26 @@ RSpec.describe "Sessions", type: :system do
   let(:user) { create(:user) }
 
   describe 'ログイン' do
-    it 'ユーザーがログイン後投稿一覧画面にリダイレクトされること' do
-      visit '/login'
-      fill_in 'メールアドレス', with: user.email
-      fill_in 'パスワード', with: 'password'
-      click_on 'login' #ヘッダーのログインに反応してしまうためid指定に変更
-      expect(current_path).to eq root_path
+    context '認証情報が正しい場合' do
+      it 'ログインできること' do
+        visit '/login'
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'password'
+        click_on 'login' #ヘッダーのログインに反応してしまうためid指定に変更(within使った方がよいかもしれない)
+        expect(current_path).to eq root_path
+        expect(page).to have_content 'ログインしました'
+      end
+    end
+
+    context '認証情報が誤りがある場合' do
+      it 'ログインできないこと' do
+        visit '/login'
+        fill_in 'メールアドレス', with: user.email
+        fill_in 'パスワード', with: 'misspassword'
+        click_on 'login' #ヘッダーのログインに反応してしまうためid指定に変更
+        expect(current_path).to eq login_path
+        expect(page).to have_content 'ログインに失敗しました'
+      end
     end
   end
 
@@ -18,6 +32,7 @@ RSpec.describe "Sessions", type: :system do
       login(user)
       click_on 'ログアウト'
       expect(current_path).to eq '/login'
+      expect(page).to have_content 'ログアウトしました'
     end
   end
 end
